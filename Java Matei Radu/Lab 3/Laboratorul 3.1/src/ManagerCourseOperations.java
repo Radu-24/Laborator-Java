6a1db866fad1f8c6f6befd12648dcef7dd62531d
LollipopMaster <115228230+LollipopMaster@users.noreply.github.com>
2024-01-11 16:27:08 +0200

    Delete Java Matei Radu directory

--- Java Matei Radu/Lab 3/Laboratorul 3.1/src/ManagerCourseOperations.java
+++
@@ -1,5 +0,0 @@
-public interface ManagerCourseOperations {
-    void AddCourse(Course course);
-    void UpdateCourse(Course course);
-    void DeleteCourse(Course course);
-}
--- Java Matei Radu/Lab 4/src/ISourceManager.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-public interface ISourceManager {
-    List<Course> getCursuri();
-
-    List<Student> getStudenti();
-}
